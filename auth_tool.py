2fbd0ca3ba1e7fd696428f503de080a36ee337b4
davejonesbkk <davejonesbkk@gmail.com>
2017-11-19 10:11:04 +0700

    Made changes for ip checks

--- auth_tool.py
+++ auth_tool.py
@@ -1,17 +1,22 @@
+
+#Get each domains DA score, total linking domains, ip address
+#Use Pandas to put into a dataframe and then save to csv
+
+
 from mozscape import Mozscape
-import csv, time, math, argparse
+import csv, time, math, argparse, socket
+
+import pandas as pd
+
 
 def parse_sites(sites_file):
 
 	#get the list of websites for testing
 	with open(sites_file) as sl:
-		sites = [x.strip() for x in sl.readlines()]
-
-	#create an empty list to store the websites
-	sites_list = []
-
-	for s in sites:
-		sites_list.append(s)
+		sites = [x.strip().lstrip('http://').rstrip('/') for x in sl.readlines()]
+		
+	print(sites)	
+	
 
 	#get MOZ API credentials
 	with open('keys.txt') as fp:
@@ -24,37 +29,65 @@ def parse_sites(sites_file):
 	client = Mozscape(moz_id, moz_key)
 
 	#creating new list for storing websites MOZ DA data
-	sites_data = []
+	sites_da = []
+	sites_backlinks = []
+	sites_ips = []
 
-	for i in sites_list:
+	for i in sites:
 
 			print 'Getting DA data for ', i
 			i = client.urlMetrics([i], Mozscape.UMCols.domainAuthority)
-			sites_data.append(math.ceil(i[0]['pda']))
+			sites_da.append(math.ceil(i[0]['pda']))
 			print 'Complete. Going to next domain in list'
 			print '...'
 			time.sleep(5)
+			
+	for k in sites:		
+			
+		print 'Getting total backlinks for ', k 
+		k = client.urlMetrics([k], Mozscape.UMCols.links)
+		sites_backlinks.append(k[0]['uid'])
+		print 'Complete. Going to next domain in list'
+		print '...'
+		time.sleep(5)		
+		
+	for s in sites:
+	
+		try:
+	
+			print 'Getting the ip address of ', s 
+			s = socket.gethostbyname(s)
+			sites_ips.append(s)
+			
+		except:
+			print 'Could not get ip address for ', s
+			sites_ips.append('Error getting ip')
+			continue
+			
+	
+			
 
-
-	#create a dictionary for storing the websites and their MOZ metrics
-	sites_metrics = dict(zip(sites_list, sites_data))
+	#create a dictionary for storing the websites, their DA score and number of backlinks
+	sites_metrics = dict(zip(sites, zip(sites_da, sites_backlinks, sites_ips)))
 
 	print sites_metrics
 
+	
+	
+	df = pd.DataFrame.from_dict(data=sites_metrics, orient='index')
+	df.columns = ['Domain Authority', 'Backlinks', 'IP']
+	
+	print(df)
+
+	to_file(df)
+
+
+def to_file(df):
+
 	print 'Saving to file'
 
-	to_file(sites_metrics)
-
-
-def to_file(sites_metrics):
-
-	#save websites and data to csv file		
-	with open('sites_data.csv', 'wb') as sd:
-		writer = csv.writer(sd) 
-		writer.writerow(["Domain", "Domain Authority"])
-		for row in sites_metrics.iteritems():
-		#writer.writeheader()
-			writer.writerow(row)
+	df.to_csv('sites_data.csv')
+	
 
 def main():
 
